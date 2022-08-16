566774a469c932875bbb5e7c0d873da28b8d35aa
karl-no <karl.publicidade@gmail.com>
2022-08-16 18:57:54 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa se o topo contém um conjunto fixo de links de navegação', () => {
+  test('Testa se é exibido na tela um link com o texto Home', () => {
+    // acessar
+    renderWithRouter(<App />);
+
+    // aferir
+    const homeTitle = screen.getByRole('link', { name: /home/i });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido na tela um link com o texto About', () => {
+    // acessar
+    renderWithRouter(<App />);
+
+    // aferir
+    const homeTitle = screen.getByRole('link', { name: /about/i });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido na tela um link com o texto Favorite Pokémons', () => {
+    // acessar
+    renderWithRouter(<App />);
+
+    // aferir
+    const homeTitle = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(homeTitle).toBeInTheDocument();
+  });
+});
