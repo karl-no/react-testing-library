1251ecc07d7dddc39899c0e90263328056fe57a0
karl-no <karl.publicidade@gmail.com>
2022-08-17 15:36:01 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  test('É exibido na tela um h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const titleNotFound = screen.getByRole('heading', { name: /not found/i });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('Testa se a imagem do Pikachu chorando é exibida', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img', { name: /not found/i });
+    const srcImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', srcImage);
+  });
+});
