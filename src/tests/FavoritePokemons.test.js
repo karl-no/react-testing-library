631b0c6da3f7322482d902512b414706e790767d
karl-no <karl.publicidade@gmail.com>
2022-08-17 15:13:59 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const noPokemon = screen.getByText('No favorite pokemon found');
+  test('Testa se é exibida a mensagem "No favorite pokemon found"', () => {
+    expect(noPokemon).toBeInTheDocument();
+  });
+});
